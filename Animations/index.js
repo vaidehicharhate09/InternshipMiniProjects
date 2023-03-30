8b423cfc83f203c3a3a2d7103ba8d5c2a72842f3
vaidehicharhate09 <89095262+vaidehicharhate09@users.noreply.github.com>
2023-03-30 18:29:34 +0530

    Add files via upload

---
+++ Animations/index.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#mytext',{
+    strings: ['Just my doggo story'],
+    typeSpeed : 100
+} )     //to which element we want to apply animations and # is used for id 
+// Typed takes two parameter first one is element to which animations is to be applied and second one is the property of animation
+
+AOS.init(
+    duration = 1000
+)
